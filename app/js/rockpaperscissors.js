7e8707e49470121b17945e3ab62b75449dd76c13
smctigue <stephenmctigue@gmail.com>
2015-08-03 14:34:11 -0700

    starting over... Re-wrote #getPlayerMove to include if...else statement

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -21,11 +21,21 @@ function randomPlay() {
 /*           Write Your Code Below            */
 ////////////////////////////////////////////////
 
+var playerMove;
+var computerMove;
 function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-        return move || getInput();
+    var move = getInput();
+    if (move === null || 0) {
+        getInput();
+    } else {
+        console.log("Player picked " + move + ".");
+        playerMove = move;
+        getComputerMove();
+    } 
+
 }
 
 function getComputerMove(move) {
